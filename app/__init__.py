10ea0f7a34ff61081cebd35165d7344bf060fa81
Roseanne Phillips <rphillips.rp@gmail.com>
2021-06-16 09:48:56 -0400

    set up module and testing routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
+from app.routes import home
+
 from flask import Flask
+
+
 
 def create_app(test_config=None):
   # set up app config
@@ -11,6 +15,9 @@ def create_app(test_config=None):
   @app.route('/hello')
   def hello():
     return 'hello world'
+
+  # register routes
+  app.register_blueprint(home)
 
   return app
 
